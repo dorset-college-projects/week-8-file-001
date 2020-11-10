c2ad5d437171dd69fdd2c410e6405701720c3165
John Rowley <outsidethebox@eircom.net>
2020-11-10 13:39:39 +0000

    First commit

---
+++ src/com/dorsetcollegeoop/TestDriver.java
@@ -0,0 +1,12 @@
+package com.dorsetcollegeoop;
+
+public class TestDriver {
+
+    public static void run() {
+
+        WriteToFile wf = new WriteToFile();
+
+        wf.CreateTextFile();
+
+    }
+}
---
+++ src/com/dorsetcollegeoop/WriteToFile.java
@@ -0,0 +1,43 @@
+package com.dorsetcollegeoop;
+
+import java.io.FileNotFoundException;
+import java.util.Formatter;
+
+public class WriteToFile {
+
+    private static Formatter output;
+
+    public void CreateTextFile() {
+        OpenFile();
+        AddRecords();
+        CloseFile();
+
+    }
+
+    public static void OpenFile() {
+
+        try {
+            output = new Formatter("superheroes.txt");
+        }
+        catch (SecurityException securityException) {
+            System.out.println("Write permission denied");
+            System.exit(1);
+        }
+        catch (FileNotFoundException fileNotFoundException) {
+            System.out.println("Error opening file, Terminating");
+            System.exit(1);
+        }
+
+    }
+    public static void AddRecords() {}
+    public static void CloseFile() {
+
+        if (output != null) {
+            output.close();
+        }
+
+
+    }
+
+
+}
